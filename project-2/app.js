3ddc47dda8a596615bf5ba7a0d5538633f1a3fb1
vishwas-28cpu <vishwas.work28@gmail.com>
2026-01-26 21:43:48 +0530

    twenty nine commit

--- project-2/app.js
+++ project-2/app.js
@@ -2,12 +2,12 @@ console.log("hello");
 console.log("hello");
 console.log("hello");
 console.log("hello");
-
-
-
-
-
-
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
 
 
 
